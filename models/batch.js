60625817ea06258835fc06571d11fdf937ca24b5
2Deo <ziemowit.holda@gmail.com>
2017-11-15 21:35:22 +0100

    Organizing batchSchema

--- models/batch.js
+++ models/batch.js
@@ -1,12 +1,26 @@
 const mongoose = require('../config/database')
 const { Schema } = mongoose
-const students = require('./student').schema
+const photo = "https://www.google.nl/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwjG7faytMHXAhVSL1AKHYG6D1MQjRwIBw&url=https%3A%2F%2Fwww.turbosquid.com%2F3d-model%2Fhead&psig=AOvVaw0IQggmAzb9-XfGON_JWJEy&ust=1510863701104328"
+
+
+const evaluationSchema = new Schema({
+  colorCode: { type: String, default: "green"},
+  date: { type: Date, default: Date.now },
+  description: { type: String }
+})
+
+const studentSchema = new Schema({
+  name: { type: String, required: true },
+  evaluation: [evaluationSchema],
+  photo: { type: String, default: photo },
+  batchId: { type: Number, ref: "batches"}
+})
 
 const batchSchema = new Schema({
-  batchNumber: { type: Number, required: true},
+  students: [studentSchema],
+  batchNumber: { type: Number },
   startDate: { type: String },
   endDate: { type: String },
-  students: [students]
 })
 
 module.exports = mongoose.model('batches', batchSchema)
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
-const students = require('./students')
 const users = require('./users')
 const sessions = require('./sessions')
 const batches = require('./batches')
 
 module.exports = {
-  students,
   users,
   sessions,
   batches
