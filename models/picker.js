f3aacc5feec201fda3ca3a9cfd2cbb487b60a421
2Deo <ziemowit.holda@gmail.com>
2017-11-16 23:14:40 +0100

    Working on picker algorithm

---
+++ models/picker.js
@@ -0,0 +1,76 @@
+//pick a student by the color code,
+//return a new object that is keyed by priority
+
+students.json //array
+
+var picker = students.reduce(function(all, item, index) {
+  if(item.priority === "1") {
+    all[item.priority] = item.name;
+  else
+    (item.priority === "2")
+  else
+    (item.priority === "3")
+  }
+  return all;
+},{});
+//=> 1: "Joe Gage", 1: "walter Mitty", 2: "John Ruth", 2: "Alice Doe",3: "Marry Poppins"
+
+students.filter(function(item, index) {
+  return item.priority === "1";
+})
+//picker 2 - where selection priority is set as follows:
+//1-first to be selected "red"
+//2-second "yellow"
+//3-last, means "green"
+var students = [
+    {name: "John Ruth",
+    id       : 5566,
+    photo: "http://xxx",
+    color: "yellow"
+    priority: "2"},
+
+    {name: "Walter Mitty",
+    id       : 1236,
+    photo: "http://xxx",
+    color: "yellow"
+    priority: "1"},
+
+    {name: "Alice Doe",
+    id       : 5098,
+    photo: "http://xxx",
+    color: "yellow"
+    priority: "2"},
+
+    {name: "Mary Poppins",
+    id       : 6543,
+    photo: "http://xxx",
+    color: "yellow"
+    priority: "3"},
+
+    {name: "Oswaldo Monterray",
+    id       : 0987,
+    photo: "http://xxx",
+    color: "yellow"
+    priority: "1"},
+
+    {name: "Cindy Domergue",
+    id       : 3417,
+    photo: "http://xxx",
+    color: "yellow"
+    priority: "3"},
+];
+//filter
+Array.prototype.filter = function(fn) {
+  return this.reduce(function(all, item, index) {
+    if(fn(item, index)) {
+      all.push(item);
+    }
+    return all;
+  }, []);
+}
+
+students.filter(function(item, index) {
+  return item.priority === "1";
+})
+//map
+students.map(function(item) {return item.priority});
